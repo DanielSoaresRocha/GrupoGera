ac17facf0082d40f67d7a4455cf5f235c7e8e88f
DanielSoaresRocha <legoliga123@gmail.com>
2021-07-24 00:57:44 -0300

    Gerando gráfio da média de consumo

--- src/app/modules/dashboard/inicio/inicio.component.ts
+++ src/app/modules/dashboard/inicio/inicio.component.ts
@@ -6,7 +6,10 @@ import { UnidadeConsumidora } from '@src/app/core/models/unidade-consumidora.mod
 import { FaturaService, UnidadeConsumidoraService } from '@src/app/shared/services';
 import { Observable } from 'rxjs';
 
-import { map } from 'rxjs/operators';
+interface UnidadesGraphic{
+  unidade: string;
+  mediaConsumo: number;
+}
 
 @Component({
   selector: 'app-inicio',
@@ -17,6 +20,10 @@ export class InicioComponent implements OnInit {
   unidades$: Observable<UnidadeConsumidora[]>;
   faturas$: Observable<Fatura[]>;
 
+  unidadesConsumo: UnidadesGraphic[] = null;
+  consumos: number[] = [];
+  maiorConsumo = 0;
+
   constructor(
     private unidadeConsumidoraService: UnidadeConsumidoraService,
     private faturaService: FaturaService
@@ -25,5 +32,47 @@ export class InicioComponent implements OnInit {
   ngOnInit(): void {
     this.unidades$ = this.unidadeConsumidoraService.getAll();
     this.faturas$ = this.faturaService.getAll();
+
+    this.unidades$.subscribe(
+      (unidades: UnidadeConsumidora[]) => {
+        let unidadesConsumo = [];
+
+        unidades.forEach((unidade) => {
+          let consumo = 0;
+          unidade.faturas.forEach((fatura: Fatura) => {
+            consumo += fatura.consumo;
+          })
+          consumo = (consumo / (unidade.faturas.length > 0 ? unidade.faturas.length : 1)); // Média
+          this.maiorConsumo = consumo > this.maiorConsumo ? consumo : this.maiorConsumo;
+
+          unidadesConsumo.push({
+            unidade: unidade.nome,
+            mediaConsumo: consumo
+          })
+        })
+
+        this.gerarGrafico();
+        this.unidadesConsumo = unidadesConsumo;
+      }
+    )
   }
+
+  gerarGrafico(){
+    let interator = this.maiorConsumo/5;
+    let anterior = 0;
+
+    for(let i = 0; i < 5; i++) {
+      let dado = anterior + interator;
+      this.consumos.push(dado)
+      anterior = dado;
+    }
+  }
+
+  calcularPorcentagem(value: number){
+    if(value == 0)
+      return 0
+    return ((value * 100)/ this.maiorConsumo).toFixed(0);
+  }
+
+
 }
